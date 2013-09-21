de738129e838f1c4211656f0f68f3de6957755f4
krzysztofdoroz <krzysztofdoroz@gmail.com>
2013-09-21 17:50:22 +0100

    pattern matching instead of bunch of clumsy if/else statements

--- src/scheme/parser/Parser.scala
+++ src/scheme/parser/Parser.scala
@@ -7,37 +7,17 @@ object Parser {
   }
   
   def parse(input: List[String], currentList : List[Any], stack : List[List[Any]], result: List[Any]): List[Any] = {
-      
-      if (input.length == 0 && currentList.size > 0)
-      {
-       //println(currentList)
-       result ++ currentList
-      }
-      if (input.length == 0)
-      {
-    	  result
-      }
-      else if (input.head.equals(""))
-      {
-        parse(input.tail, currentList, stack, result)
-      }
-      else if(input.head.equals("("))
-      {
-      	parse(input.tail, List() , if (currentList.isEmpty) {stack} else {stack :+ currentList}, result)
-      }
-      else if (input.head.equals(")") && stack.size == 0)
-      {
-    	  currentList
-      }
-      else if (input.head.equals(")"))
-      {
-    	  parse(input.tail, stack.last :+ currentList, stack.take(stack.size - 1) , result)
-      }
-      else 
-      {
-        parse(input.tail, currentList :+ input.head, stack, result)
-      }
+
+    (input, currentList, stack, result) match {
+      case (Nil, ch :: ct, _, _) => result ++ currentList
+      case (Nil, _, _, _) => result
+      case ("" :: it, _, _, _) => parse(it, currentList, stack, result)
+      case ("(" :: it, _, _, _) => parse(it, List(), if (currentList.isEmpty) { stack } else { stack :+ currentList }, result)
+      case (")" :: it, _, Nil, _) => currentList
+      case (")" :: it, _, _, _) => parse(input.tail, stack.last :+ currentList, stack.take(stack.size - 1), result)
+      case _ => parse(input.tail, currentList :+ input.head, stack, result)
     }
+  }
   
   def tokenize(s : String) = {
     s.replace("(", " ( ").replace(")", " ) ").split("\\s+").toList
